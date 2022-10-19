62f97282f0180336ac87547d5ed4795e7fbfc4cf
Jason <ggj00125@gmail.com>
2022-10-19 16:47:21 +0900

    Feat: Implementing Subscriptions 코드 실습

--- Mastering RxSwift in iOS/HelloRxSwift/HelloRxSwift/ViewController.swift
+++ Mastering RxSwift in iOS/HelloRxSwift/HelloRxSwift/ViewController.swift
@@ -13,6 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let observable4 = Observable.from([1, 2, 3, 4, 5])
+//        observable4.subscribe { event in
+//            if let element = event.element {
+//                print(element)
+//            }
+//        }
+//
+//        let observable3 = Observable.of([1, 2, 3])
+//        observable3.subscribe{ event in
+//            if let element = event.element {
+//                print(element)
+//            }
+//        }
+        
+        observable4.subscribe(onNext: { element in
+            print(element)
+        })
 
     }
 
