4a9ba37f6e16f7d9120f026b8333b31a50795c96
Jason <ggj00125@gmail.com>
2022-10-20 22:50:32 +0900

    Feat: Disposing and Terminating 코드 학습

--- Mastering RxSwift in iOS/HelloRxSwift/HelloRxSwift/ViewController.swift
+++ Mastering RxSwift in iOS/HelloRxSwift/HelloRxSwift/ViewController.swift
@@ -9,31 +9,28 @@ import UIKit
 import RxSwift
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let observable4 = Observable.from([1, 2, 3, 4, 5])
-//        observable4.subscribe { event in
-//            if let element = event.element {
-//                print(element)
-//            }
-//        }
-//
-//        let observable3 = Observable.of([1, 2, 3])
-//        observable3.subscribe{ event in
-//            if let element = event.element {
-//                print(element)
-//            }
-//        }
+        let disposeBag = DisposeBag()
         
-        observable4.subscribe(onNext: { element in
-            print(element)
-        })
-
+        Observable.of("A", "B", "C")
+            .subscribe {
+                print($0)
+            }.disposed(by: disposeBag)
+        
+        Observable<String>.create{ observer in
+            observer.onNext("A")
+            observer.onCompleted()
+            observer.onNext("?")
+            return Disposables.create()
+        }.subscribe(onNext: { print($0) }, onError: { print($0) },
+                    onCompleted: { print("Completed") },
+                    onDisposed: { print("Disposed") } )
+        .disposed(by: disposeBag)
+        
     }
-
-
 }
 
