bc334f9d9568226c498073718929533627d1115b
Jason <ggj00125@gmail.com>
2022-10-21 14:58:34 +0900

    Feat: ReplaySubjects 학습 코드

--- Mastering RxSwift in iOS/HelloRxSwift/HelloRxSwift/ViewController.swift
+++ Mastering RxSwift in iOS/HelloRxSwift/HelloRxSwift/ViewController.swift
@@ -16,14 +16,24 @@ class ViewController: UIViewController {
         
         let disposeBag = DisposeBag()
         
-        let subject = BehaviorSubject(value: "Initial Value")
-        subject.onNext("Last Issue")
-        
-        subject.subscribe{ event in
-            print(event)
-        }
+        let subject = ReplaySubject<String>.create(bufferSize: 2)
         
         subject.onNext("Issue 1")
+        subject.onNext("Issue 2")
+        subject.onNext("Issue 3")
+        
+        subject.subscribe{
+            print($0)
+        }
+        
+        subject.onNext("Issue 4")
+        subject.onNext("Issue 5")
+        subject.onNext("Issue 6")
+        
+        print("[Subscription 2]")
+        subject.subscribe{
+            print($0)
+        }
     }
 }
 
