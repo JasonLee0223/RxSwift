c83400935641757b23714cb426995348c83214bd
Jason <ggj00125@gmail.com>
2022-10-25 01:16:35 +0900

    Feat: Displaying Photos from Photo Library 학습 코드

---
+++ Mastering RxSwift in iOS/CameraFilter/CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by Jason on 2022/10/25.
+//  Copyright © 2022 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
